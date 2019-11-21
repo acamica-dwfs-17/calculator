7d7808e011fe45608727e572cd3fd33b07c03760
Ramiro Yaben <ramiroyaben@gmail.com>
2019-11-21 19:31:26 -0300

    Initial commit

---
+++ script.js
@@ -0,0 +1,70 @@
+var resultado;
+var acumulador1 = '';
+var acumulador2 = '';
+var operacion = '';
+
+
+function setearOperador(operador) {
+  resultado = hacerOperacion(acumulador1, acumulador2, operador);
+  if (resultado)
+    actualizarDisplay(resultado);
+
+  if (operador && resultado) {
+    acumulador1 = resultado;
+    acumulador2 = '';
+  }
+  operacion = operador;
+}
+
+function setearValor(valor) {
+  if (!operacion) {
+    acumulador1 += valor;
+    actualizarDisplay(acumulador1);
+  } else {
+    acumulador2 += valor;
+    actualizarDisplay(acumulador2);
+  }
+}
+
+function actualizarDisplay(nuevoValor) {
+  console.log(nuevoValor)
+  var display = document.getElementById('viewer');
+  display.innerText = nuevoValor;
+}
+
+function hacerOperacion(acu1, acu2, oper) {
+  let resultadoParcial;
+  if (acu1 && acu2) {
+    var numeroParseado1 = parseFloat(acu1);
+    var numeroParseado2 = parseFloat(acu2);
+    resultadoParcial = Operacion(numeroParseado1, numeroParseado2, oper)
+  }
+  return resultadoParcial
+}
+
+function Operacion(numeroParseado1, numeroParseado2, oper){
+  let resultadoParcial
+  switch (oper) {
+    case '+':
+      resultadoParcial = numeroParseado1 + numeroParseado2;
+      break;
+    case '-':
+      resultadoParcial = numeroParseado1 - numeroParseado2;
+      break;
+    case '*':
+      resultadoParcial = numeroParseado1 * numeroParseado2;
+      break;
+    case '/':
+      resultadoParcial = numeroParseado1 / numeroParseado2;
+      break;
+  }
+
+  return resultadoParcial
+}
+
+function resetear() {
+  acumulador1 = '';
+  acumulador2 = '';
+  operacion = '';
+  actualizarDisplay('');
+}
---
+++ testing.js
@@ -0,0 +1,45 @@
+function testMultiplicar() {
+    //6x8x12 = 576
+    let resultado = 0
+
+    resultado = hacerOperacion(2, 4, '*')
+    console.log(resultado)
+
+    resultado = hacerOperacion(resultado, 2, '*')
+    console.log(resultado)
+
+    return resultado === 16
+}
+
+function testDividir() {
+    //6x8x12 = 576
+    let resultado = 0
+
+    resultado = hacerOperacion(10, 2, '/')
+
+    resultado = hacerOperacion(resultado, 5, '/')
+
+    return resultado === 1
+}
+
+function testRestar() {
+    //6x8x12 = 576
+    let resultado = 0
+
+    resultado = hacerOperacion(10, 2, '-')
+
+    resultado = hacerOperacion(resultado, 5, '-')
+
+    return resultado === 3
+}
+
+function testSumar() {
+    //6x8x12 = 576
+    let resultado = 0
+
+    resultado = hacerOperacion(2, 2, '+')
+
+    resultado = hacerOperacion(resultado, 2, '+')
+
+    return resultado === 6
+}
