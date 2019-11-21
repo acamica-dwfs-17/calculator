6e6c34fe1a55e4634b5734486a1c10a855f638ad
Ramiro Yaben <ramiroyaben@gmail.com>
2019-11-21 19:43:28 -0300

    Initial commit

--- testing.js
+++ testing.js
@@ -2,21 +2,44 @@ function testMultiplicar() {
     //6x8x12 = 576
     let resultado = 0
 
-    resultado = hacerOperacion(6, 8, '*')
+    resultado = hacerOperacion(2, 4, '*')
+    console.log(resultado)
 
-    resultado = hacerOperacion(resultado, 12, '*')
+    resultado = hacerOperacion(resultado, 2, '*')
+    console.log(resultado)
 
-    return resultado === 576
+    return resultado === 16
 }
 
 function testDividir() {
+    //6x8x12 = 576
+    let resultado = 0
 
+    resultado = hacerOperacion(10, 2, '/')
+
+    resultado = hacerOperacion(resultado, 5, '/')
+
+    return resultado === 1
 }
 
 function testRestar() {
+    //6x8x12 = 576
+    let resultado = 0
 
+    resultado = hacerOperacion(10, 2, '-')
+
+    resultado = hacerOperacion(resultado, 5, '-')
+
+    return resultado === 3
 }
 
 function testSumar() {
+    //6x8x12 = 576
+    let resultado = 0
 
+    resultado = hacerOperacion(2, 2, '+')
+
+    resultado = hacerOperacion(resultado, 2, '+')
+
+    return resultado === 6
 }
