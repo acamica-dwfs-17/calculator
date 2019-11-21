e34a39dec99d50cb3f35b1283fdc41440b6cde6b
Leonel Alexander Vieyra <leonel.vieyra@mercadolibre.com>
2019-11-21 20:27:57 -0300

    Add base for testing

--- testing.js
+++ testing.js
@@ -1,45 +1,53 @@
+const { expect } = chai;
+
+describe('Prueba calculadora', () =>
+  it('debería multiplicar', () => {
+    let resultadoFinal = 9;
+    expect(hacerOperacion(3, 3, '*')).to.be.equal(resultadoFinal);
+  }));
+
 function testMultiplicar() {
-    //6x8x12 = 576
-    let resultado = 0
+  //6x8x12 = 576
+  let resultado = 0;
 
-    resultado = hacerOperacion(2, 4, '*')
-    console.log(resultado)
+  resultado = hacerOperacion(2, 4, '*');
+  console.log(resultado);
 
-    resultado = hacerOperacion(resultado, 2, '*')
-    console.log(resultado)
+  resultado = hacerOperacion(resultado, 2, '*');
+  console.log(resultado);
 
-    return resultado === 16
+  return resultado === 16;
 }
 
 function testDividir() {
-    //6x8x12 = 576
-    let resultado = 0
+  //6x8x12 = 576
+  let resultado = 0;
 
-    resultado = hacerOperacion(10, 2, '/')
+  resultado = hacerOperacion(10, 2, '/');
 
-    resultado = hacerOperacion(resultado, 5, '/')
+  resultado = hacerOperacion(resultado, 5, '/');
 
-    return resultado === 1
+  return resultado === 1;
 }
 
 function testRestar() {
-    //6x8x12 = 576
-    let resultado = 0
+  //6x8x12 = 576
+  let resultado = 0;
 
-    resultado = hacerOperacion(10, 2, '-')
+  resultado = hacerOperacion(10, 2, '-');
 
-    resultado = hacerOperacion(resultado, 5, '-')
+  resultado = hacerOperacion(resultado, 5, '-');
 
-    return resultado === 3
+  return resultado === 3;
 }
 
 function testSumar() {
-    //6x8x12 = 576
-    let resultado = 0
+  //6x8x12 = 576
+  let resultado = 0;
 
-    resultado = hacerOperacion(2, 2, '+')
+  resultado = hacerOperacion(2, 2, '+');
 
-    resultado = hacerOperacion(resultado, 2, '+')
+  resultado = hacerOperacion(resultado, 2, '+');
 
-    return resultado === 6
+  return resultado === 6;
 }
